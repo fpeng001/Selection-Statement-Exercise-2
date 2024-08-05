57317ee1d9439a6252b3d4f37a774e2183fd302c
Fu-Lin Peng <fulinpeng53@gmail.com>
2024-08-05 00:08:34 -0400

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favortie school subject?");
+            string subject = (Console.ReadLine());
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is cool");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is cool");
+                    break;
+                case "pe":
+                    Console.WriteLine("P.E. is not my thing");
+                    break;
+                case "history":
+                    Console.WriteLine("History is okay");
+                    break;
+                case "english":
+                    Console.WriteLine("English is epic");
+                    break;
+                default:
+                    Console.WriteLine("I don't know what that is.");
+                    break; 
+            }
+
         }
     }
 }
